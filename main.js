05fe0a4a9803586c026962f3f02c0055182c7ea3
AbdulazizKhojinigmonov <dont89r@gmail.com>
2025-01-23 17:00:57 +0500

    base

---
+++ main.js
@@ -0,0 +1,32 @@
+const body = document.querySelector("body");
+
+const themeBtn = document.querySelector("#themeChange");
+
+themeBtn.addEventListener("click", themeChange)
+function themeChange(){
+    body.classList.toggle("dark")
+}
+
+
+async function sendRequest(url, method, data) {
+    if (method == "POST") {
+      let response = await fetch(url, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      response = await response.json();
+      return response;
+    } else if (method == "GET") {
+      url = url + "?" + new URLSearchParams(data);
+      let response = await fetch(url, {
+        method: "GET",
+      });
+      response = await response.json();
+      return response;
+    }
+  }
